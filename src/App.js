56f11945c2df2921ee2d3d22a162c772f62f429d
gwarz <gmwarzecha@tutanota.com>
2021-04-27 15:40:11 -0700

    added Task component and ability to delete tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { useState } from 'react';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 const App = () => {
- 
+// by placing the object within Tasks rather than outside of the component, it becomes part of the component state which can then be changed using setTasks
+// state is immutable and needs to be re-created and sent back 
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1, 
+        text: 'Homework',
+        day: 'Apr 27th at 1430',
+        reminder: false
+      },
+      {
+        id: 2, 
+        text: 'Dinner',
+        day: 'Apr 27th at 1530',
+        reminder: false
+      },
+      {
+        id: 3, 
+        text: 'Tutorial',
+        day: 'Apr 27th at 1900',
+        reminder: true
+      }
+    ]
+  )
+
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete=
+      {deleteTask} /> : 'Nothing to Show Here!'}
+      
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,35 +1,12 @@
-import { useState } from 'react';
+import Task from './Task'
 
-// by placing the object within Tasks rather than outside of the component, it becomes part of the component state which can then be changed using setTasks
-// state is immutable and needs to be re-created and sent back 
-const Tasks = () => {
-  const [tasks, setTasks] = useState(
-    [
-      {
-        id: 1, 
-        text: 'Homework',
-        day: 'Apr 27th at 1430',
-        reminder: false
-      },
-      {
-        id: 2, 
-        text: 'Dinner',
-        day: 'Apr 27th at 1530',
-        reminder: false
-      },
-      {
-        id: 3, 
-        text: 'Tutorial',
-        day: 'Apr 27th at 1900',
-        reminder: true
-      }
-    ]
-  )
 
+const Tasks = ({ tasks, onDelete }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} 
+        onDelete={onDelete}/>
       ))}
     </>
   )
