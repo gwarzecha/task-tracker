120eaaed8e319fe42c4c0af3fa54e39ade3a1061
gwarz <gmwarzecha@tutanota.com>
2021-04-27 16:41:30 -0700

    created ability to add a task to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
   // by placing the object within Tasks rather than outside of the component, it becomes part of the component state which can then be changed using setTasks
@@ -28,6 +29,14 @@ const App = () => {
     ]
   )
 
+  // add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+
+  }
+
   // delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -48,6 +57,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete=
           {deleteTask} onToggle={toggleReminder} />
