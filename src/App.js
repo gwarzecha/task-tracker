82f71970f407d9b843d9056ec38cc17e67c91e15
gwarz <gmwarzecha@tutanota.com>
2021-04-27 16:12:50 -0700

    added onDoubleClick to indicate priority of tasks

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 const App = () => {
-// by placing the object within Tasks rather than outside of the component, it becomes part of the component state which can then be changed using setTasks
-// state is immutable and needs to be re-created and sent back 
+  // by placing the object within Tasks rather than outside of the component, it becomes part of the component state which can then be changed using setTasks
+  // state is immutable and needs to be re-created and sent back 
   const [tasks, setTasks] = useState(
     [
       {
-        id: 1, 
+        id: 1,
         text: 'Homework',
         day: 'Apr 27th at 1430',
         reminder: false
       },
       {
-        id: 2, 
+        id: 2,
         text: 'Dinner',
         day: 'Apr 27th at 1530',
         reminder: false
       },
       {
-        id: 3, 
+        id: 3,
         text: 'Tutorial',
         day: 'Apr 27th at 1900',
         reminder: true
@@ -33,12 +33,26 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? {
+          ...task, reminder:
+            !task.reminder
+        } : task
+      )
+    )
+  }
+
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete=
-      {deleteTask} /> : 'Nothing to Show Here!'}
-      
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete=
+          {deleteTask} onToggle={toggleReminder} />
+      ) : ('Nothing to Show Here!'
+      )}
     </div>
   )
 }
